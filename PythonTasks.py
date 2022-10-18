0bb51a76281b75c4ca3cca1705a2f9f601e6bc4b
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2022-10-18 18:51:44 +0400

    Update

--- PythonTasks.py
+++ PythonTasks.py
@@ -1855,3 +1855,36 @@ student_names = ['Camila Rodriguez', 'Juan Cruz', 'Dan Richards', 'Sam Boyle', '
 student_grades = [86, 98, 89, 92, 45, 67, 89, 90, 100, 98, 10, 96, 93]
 
 result = [{student_ids[i]: {student_names[i]: student_grades[i]}} for i in range(len(student_ids))]
+
+
+
+"""
+Дополните приведенный код, чтобы в списках значений элементов словаря my_dict  не было чисел, больших 2020. При этом порядок оставшихся элементов меняться не должен.
+
+Примечание. Необходимо изменить словарь my_dict, выводить ничего не надо.
+"""
+
+my_dict = {'C1': [10, 20, 30, 7, 6, 23, 90], 'C2': [20, 30, 40, 1, 2, 3, 90, 12], 'C3': [12, 34, 20, 21], 'C4': [22, 54, 209, 21, 7], 'C5': [2, 4, 29, 21, 19], 'C6': [4, 6, 7, 10, 55], 'C7': [4, 8, 12, 23, 42], 'C8': [3, 14, 15, 26, 48], 'C9': [2, 7, 18, 28, 18, 28]}
+result = {key : [i for i in value if i <= 20] for key, value in my_dict.items()}
+
+for sub in my_dict:
+    if sub in result:
+        my_dict[sub] = result[sub]
+print(my_dict)
+
+
+"""
+Словарь emails содержит информацию об электронных адресах пользователей, сгруппированных по домену. Дополните приведенный код, чтобы он вывел все электронные адреса в алфавитном порядке, каждый на отдельной строке, в формате username@domain.
+
+Примечание 1. Для сортировки в алфавитном порядке используйте встроенную функцию sorted(), либо списочный метод sort().
+
+Примечание 2. Группировать электронные адреса по доменам не нужно.
+"""
+
+emails = {'nosu.edu': ['timyr', 'joseph', 'svetlana.gaeva', 'larisa.mamuk'],
+'gmail.com': ['ruslan.chaika', 'rustam.mini', 'stepik-best'],
+'msu.edu': ['apple.fruit', 'beegeek', 'beegeek.school'],
+'yandex.ru': ['surface', 'google'],
+'hse.edu': ['tomas-henders', 'cream.soda', 'zivert'],
+'mail.ru': ['angel.down', 'joanne', 'the.fame.moster']}
+print(*sorted({i + '@' + k for k, v in emails.items() for i in v}), sep = '\n')

c6ec17403bacd71443d281cc4e2138128ef30075
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2022-11-08 21:03:50 +0400

    Update

--- PythonTasks.py
+++ PythonTasks.py
@@ -2797,3 +2797,28 @@ file = file_open.readlines()
 
 print(''.join(file[-2].strip()))
 file_open.close()
+
+
+
+"""
+Вам доступен текстовый файл lines.txt, в котором записаны строки текста. Напишите программу, которая выводит все строки наибольшей длины из файла, не меняя их порядок.
+
+Формат входных данных
+На вход программе ничего не подается.
+
+Формат выходных данных
+Программа должна вывести строки указанного файла, имеющие наибольшую длину, не меняя их порядка.
+"""
+max_len = 0
+max_str = []
+
+with open('lines.txt') as file:
+    for line in file.readlines():
+        if len(line) > max_len:
+            max_len = len(line)
+            max_str = [line]
+        elif len(line) == max_len:
+            max_str.append(line)
+            
+for elem in max_str:
+    print(elem, end = '')

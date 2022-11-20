884544777598a8dd444f95c58e0367736aa6c5a3
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2022-11-20 17:47:08 +0400

    Update

--- PythonOOP.py
+++ PythonOOP.py
@@ -690,3 +690,126 @@ class SingletonFive:
         self.name = name
         
 objs = [SingletonFive(str(n)) for n in range(10)]
+
+
+"""
+Подвиг 8. В программе объявлена переменная TYPE_OS и два следующих класса:
+
+TYPE_OS = 1 # 1 - Windows; 2 - Linux
+
+class DialogWindows:
+    name_class = "DialogWindows"
+
+
+class DialogLinux:
+    name_class = "DialogLinux"
+Необходимо объявить третий класс с именем Dialog, который бы создавал объекты командой:
+
+dlg = Dialog(<название>)
+Здесь <название> - это строка, которая сохраняется в локальном свойстве name объекта dlg.
+
+Класс Dialog должен создавать объекты класса DialogWindows, если переменная TYPE_OS = 1 и объекты класса DialogLinux, если переменная TYPE_OS не равна 1. При этом, переменная TYPE_OS может меняться в последующих строчках программы. Имейте это в виду, при объявлении класса Dialog.
+
+P.S. В программе на экран ничего выводить не нужно. Только объявить класс Dialog.
+"""
+TYPE_OS = 1 # 1 - Windows; 2 - Linux
+
+class DialogWindows:
+    name_class = "DialogWindows"
+    
+
+class DialogLinux:
+    name_class = "DialogLinux"
+    
+    
+class Dialog:
+    
+    def __new__(cls, *args, **kwargs):
+        obj = None
+        if TYPE_OS == 1:
+            obj = super().__new__(DialogWindows)
+        else:
+            obj = super().__new__(DialogLinux)
+            
+        obj.name = args[0]
+        return obj
+
+
+"""
+одвиг 9 (на повторение материала). Объявите класс Point для представления точек на плоскости. Создавать объекты этого класса предполагается командой:
+
+pt = Point(x, y)
+Здесь x, y - числовые координаты точки на плоскости (числа), то есть, в каждом объекте этого класса создаются локальные свойства x, y, которые хранят конкретные координаты точки.
+
+Необходимо в классе Point реализовать метод clone(self), который бы создавал новый объект класса Point как копию текущего объекта с локальными атрибутами x, y и соответствующими значениями.
+
+Создайте в программе объект pt класса Point и еще один объект pt_clone через вызов метода clone.
+
+P.S. В программе на экран ничего выводить не нужно.
+"""
+class Point:
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+    def clone(self):
+        return Point(self.x, self.y)
+        
+pt = Point(10,20)
+pt_clone = Point.clone(pt)
+
+
+"""
+Подвиг 10 (на повторение материала). В программе предполагается реализовать парсер (обработчик) строки (string) в определенный выходной формат. Для этого объявлен следующий класс:
+
+class Loader:
+    def parse_format(self, string, factory):
+        seq = factory.build_sequence()
+        for sub in string.split(","):
+            item = factory.build_number(sub)
+            seq.append(item)
+
+        return seq
+
+И предполагается его использовать следующим образом:
+
+ld = Loader()
+res = ld.parse_format("4, 5, -6.5", Factory())
+На выходе (в переменной res) ожидается получить список из набора вещественных чисел. Например, для заданной строки, должно получиться:
+
+[4.0, 5.0, -6.5]
+
+Для реализации этой идеи необходимо вначале программы прописать класс Factory с двумя методами:
+
+build_sequence(self) - для создания начального пустого списка (метод должен возвращать пустой список);
+build_number(self, string) - для преобразования переданной в метод строки (string) в вещественное значение (метод должен возвращать полученное вещественное число).
+
+Объявите класс с именем Factory, чтобы получать на выходе искомый результат.
+"""
+
+
+class Factory:
+    def build_sequence(self):
+        self.a = []
+        return self.a
+        
+    def build_number(self, string):
+        return float(string)
+
+
+class Loader:
+   
+    def parse_format(self, string, factory):
+        seq = factory.build_sequence()
+        for sub in string.split(","):
+            item = factory.build_number(sub)
+            seq.append(item)
+
+        return seq
+
+
+# эти строчки не менять!
+ld = Loader()
+s = input()
+res = ld.parse_format(s, Factory())

189bc23a455fdc37132435124a43cdc5d19d1c74
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2022-11-29 22:41:00 +0400

    Update

--- PythonOOP.py
+++ PythonOOP.py
@@ -1692,3 +1692,198 @@ class EmailValidator :
     @staticmethod       
     def __is_email_str(email):
         return type(email) == str
+
+"""
+Подвиг 4. Объявите в программе класс Car, в котором реализуйте объект-свойство с именем model для записи и считывания информации о модели автомобиля из локальной приватной переменной __model.
+
+Объект-свойство объявите с помощью декоратора @property. Также в объекте-свойстве model должны быть реализованы проверки:
+
+- модель автомобиля - это строка;
+- длина строки модели должна быть в диапазоне [2; 100].
+
+Если проверка не проходит, то локальное свойство __model остается без изменений.
+
+Объекты класса Car предполагается создавать командой:
+
+car = Car()
+и далее работа с объектом-свойством, например:
+
+car.model = "Toyota"
+P.S. В программе объявить только класс. На экран ничего выводить не нужно. 
+"""
+class Car:
+    def __init__(self, model=None):
+        self.__model = model
+        
+    @property
+    def model(self):
+        return self.__model
+    
+    @model.setter
+    def model (self, model):
+        if type(model) == str and (2 <len(model) < 100):
+            self.__model = model
+            
+car = Car()
+car.model = "Toyota"
+
+
+"""
+Подвиг 5. Объявите в программе класс WindowDlg, объекты которого предполагается создавать командой:
+
+wnd = WindowDlg(заголовок окна, ширина, высота)
+В каждом объекте класса WindowDlg должны создаваться приватные локальные атрибуты:
+
+__title - заголовок окна (строка);
+__width, __height - ширина и высота окна (числа).
+
+В классе WindowDlg необходимо реализовать метод:
+
+show() - для отображения окна на экране (выводит в консоль строку в формате: "<Заголовок>: <ширина>, <высота>", например "Диалог 1: 100, 50").
+
+Также в классе WindowDlg необходимо реализовать два объекта-свойства:
+
+width - для изменения и считывания ширины окна;
+height - для изменения и считывания высоты окна.
+
+При изменении размеров окна необходимо выполнять проверку:
+
+- переданное значение является целым числом в диапазоне [0; 10000].
+
+Если хотя бы один размер изменился (высота или ширина), то следует выполнить автоматическую перерисовку окна (вызвать метод show()). При начальной инициализации размеров width, height вызывать метод show() не нужно.
+
+P.S. В программе нужно объявить только класс с требуемой функциональностью.
+"""
+class WindowDlg:
+    def __init__(self, title, width=None, height=None):
+        self.__title = title
+        self.__width = width
+        self.__height = height 
+        
+    @property
+    def width(self):
+        return self.__width
+        
+    @width.setter
+    def width(self, value):
+        if type(value) == int and 0 <= value <= 10000:
+            self.__width = value
+            self.show()
+            
+    @property
+    def height(self):
+        return self.__height
+        
+    @height.setter
+    def height(self, value):
+        if type(value) == int and 0 <= value <= 10000:
+            self.__height = value
+            self.show()
+            
+    def show(self):
+        print (f'{self.__title}: {self.__width}, {self.__height}')
+
+
+"""
+Подвиг 6. Реализуйте односвязный список (не список Python, не использовать список Python для хранения объектов), когда один объект ссылается на следующий и так по цепочке до последнего:
+
+
+
+Для этого объявите в программе два класса: 
+
+StackObj - для описания объектов односвязного списка;
+Stack - для управления односвязным списком.
+
+Объекты класса StackObj предполагается создавать командой:
+
+obj = StackObj(данные)
+Здесь данные - это строка с некоторым содержимым. Каждый объект класса StackObj должен иметь следующие локальные приватные атрибуты:
+
+__data - ссылка на строку с данными, указанными при создании объекта;
+__next - ссылка на следующий объект класса StackObj (при создании объекта принимает значение None).
+
+Также в классе StackObj должны быть объявлены объекты-свойства:
+
+next - для записи и считывания информации из локального приватного свойства __next;
+data - для записи и считывания информации из локального приватного свойства __data.
+
+При записи необходимо реализовать проверку, что __next будет ссылаться на объект класса StackObj или значение None. Если проверка не проходит, то __next остается без изменений.
+
+Класс Stack предполагается использовать следующим образом:
+
+st = Stack() # создание объекта односвязного списка
+В объектах класса Stack должен быть локальный публичный атрибут:
+
+top - ссылка на первый добавленный объект односвязного списка (если список пуст, то top = None).
+
+А в самом классе Stack следующие методы:
+
+push(self, obj) - добавление объекта класса StackObj в конец односвязного списка;
+pop(self) - извлечение последнего объекта с его удалением из односвязного списка;
+get_data(self) - получение списка из объектов односвязного списка (список из строк локального атрибута __data каждого объекта в порядке их добавления, или пустой список, если объектов нет).
+
+Пример использования классов Stack и StackObj (эти строчки в программе писать не нужно):
+"""
+class StackObj:
+    def __init__(self, data):
+        self.__data = data
+        self.__next = None
+        
+    @property
+    def data(self):
+        return self.__data
+    
+    @data.setter
+    def data(self, value):
+        self.__data = value
+
+    @property
+    def next(self):
+        return self.__next
+    
+    @next.setter
+    def next(self, obj):
+        if isinstance(obj, StackObj) or obj is None:
+            self.__next = obj
+            
+class Stack:
+    def __init__(self):
+        self.top = None
+        self.last = None
+        
+    def push(self, obj):
+        if self.last:
+            self.last.next = obj
+        self.last = obj
+        if self.top is None:
+            self.top = obj
+            
+    def pop(self):
+        h = self.top
+        if h is None:
+            return
+        while h and h.next != self.last:
+            h = h.next
+        if h:
+            h.next = None
+        last = self.last
+        self.last = h
+        if self.last is None:
+            self.top = None
+            
+        return last
+        
+    def get_data(self):
+        s = []
+        h = self.top
+        while h:
+            s.append(h.data)
+            h = h.next
+        return s
+        
+st = Stack()
+st.push(StackObj("obj1"))
+st.push(StackObj("obj2"))
+st.push(StackObj("obj3"))
+st.pop()
+res = st.get_data()    # ['obj1', 'obj2']

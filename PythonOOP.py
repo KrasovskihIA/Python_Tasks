93e7a67135fd61962e3de31df11978c9b3a72c67
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2022-12-07 21:31:22 +0400

    Update

--- PythonOOP.py
+++ PythonOOP.py
@@ -1887,3 +1887,72 @@ st.push(StackObj("obj2"))
 st.push(StackObj("obj3"))
 st.pop()
 res = st.get_data()    # ['obj1', 'obj2']
+
+
+
+"""
+Подвиг 7. Объявите класс RadiusVector2D, объекты которого должны создаваться командами:
+
+v1 = RadiusVector2D()        # радиус-вектор с координатами (0; 0)
+v2 = RadiusVector2D(1)       # радиус-вектор с координатами (1; 0)
+v3 = RadiusVector2D(1, 2)    # радиус-вектор с координатами (1; 2)
+В каждом объекте класса RadiusVector2D должны формироваться локальные приватные атрибуты:
+
+__x, __y - координаты конца вектора (изначально значения равны 0, если не передано какое-либо другое).
+
+В классе RadiusVector2D необходимо объявить два объекта-свойства:
+
+x - для изменения и считывания локального атрибута __x;
+y - для изменения и считывания локального атрибута __y.
+
+При инициализации и изменении локальных атрибутов, необходимо проверять корректность передаваемых значений:
+
+- значение должно быть числом (целым или вещественным) в диапазоне [MIN_COORD; MAX_COORD].
+
+Если проверка не проходит, то координаты не меняются (напомню, что при инициализации они изначально равны 0). Величины MIN_COORD = -100, MAX_COORD = 1024 задаются как публичные атрибуты класса RadiusVector2D.
+
+Также в классе RadiusVector2D необходимо объявить статический метод:
+
+norm2(vector) - для вычисления квадратической нормы vector - переданного объекта класса RadiusVector2D (квадратическая норма вектора: x*x + y*y).
+
+P.S. В программе требуется объявить только класс. На экран ничего выводить не нужно.
+"""
+
+class RadiusVector2D:
+    MIN_COORD = -100
+    MAX_COORD = 1024
+
+
+    @classmethod
+    def __is_verify(cls, value):
+        return type(value) in (int, float) and cls.MIN_COORD <= value <= cls.MAX_COORD
+        
+    def __init__(self, x=0, y=0):
+        self.__x = self.__y = 0
+        if self.__is_verify(x):
+            self.__x = x
+        if self.__is_verify(y):
+            self.__y = y
+    
+
+    @staticmethod
+    def norm2(vector):
+        return vector.x * vector.x + vector.y * vector.y 
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if self.__is_verify(value):
+            self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if self.__is_verify(value):
+            self.__y = value

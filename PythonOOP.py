d36d3dd5cb1f6d3cf02d04466ac57d74a0eba860
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2022-12-14 21:31:09 +0400

    Update

--- PythonOOP.py
+++ PythonOOP.py
@@ -2589,3 +2589,178 @@ class Book:
 
 book = Book("Python ООП", "Сергей Балакирев", 123, 2022)
     
+
+"""
+Подвиг 4. Вы создаете интернет-магазин. Для этого нужно объявить два класса:
+
+Shop - класс для управления магазином в целом;
+Product - класс для представления отдельного товара.
+
+Объекты класса Shop следует создавать командой:
+
+shop = Shop(название магазина)
+В каждом объекте класса Shop должно создаваться локальное свойство:
+
+goods - список товаров (изначально список пустой).
+
+А также в классе объявить методы:
+
+add_product(self, product) - добавление нового товара в магазин (в конец списка goods);
+remove_product(self, product) - удаление товара product из магазина (из списка goods);
+
+Объекты класса Product следует создавать командой:
+
+p = Product(название, вес, цена)
+В них автоматически должны формироваться локальные атрибуты:
+
+id - уникальный идентификационный номер товара (генерируется автоматически как целое положительное число от 1 и далее);
+name - название товара (строка);
+weight - вес товара (целое или вещественное положительное число);
+price - цена (целое или вещественное положительное число).
+
+В классе Product через магические методы (подумайте какие) осуществить проверку на тип присваиваемых данных локальным атрибутам объектов класса (например, id - целое число, name - строка и т.п.). Если проверка не проходит, то генерировать исключение командой:
+
+raise TypeError("Неверный тип присваиваемых данных.")
+Также в классе Product с помощью магического(их) метода(ов) запретить удаление локального атрибута id. При попытке это сделать генерировать исключение:
+
+raise AttributeError("Атрибут id удалять запрещено.")
+"""
+class Shop:
+    def __init__(self, name):
+        self.name = name
+        self.goods = []
+
+    def add_product(self, product):
+        self.goods.append(product)
+
+    def remove_product(self, product):
+        self.goods.remove(product)
+
+
+
+class Product:
+    _id_instance = 1
+    
+    def __init__(self, name, weight, price ):
+        self.id = Product._id_instance
+        Product._id_instance +=1
+        
+        self.name = name
+        self.weight = weight
+        self.price = price
+
+    def __setattr__(self, key, value):
+        d = {'name': (str,), 'weight': (float, int), 'price': (float, int), 'id': (int,)}
+        if type(value) not in d[key] or type(value) in (int, float) and value < 0:
+            raise TypeError("Неверный тип присваиваемых данных.")
+        else:
+            object.__setattr__(self, key, value)
+
+    def __delattr__(self, item):
+        if item == "id":
+            raise AttributeError("Атрибут id удалять запрещено.")
+
+
+
+"""
+Подвиг 5. Необходимо создать программу для обучающего курса. Для этого объявляются три класса:
+
+Course - класс, отвечающий за управление курсом в целом;
+Module - класс, описывающий один модуль (раздел) курса;
+LessonItem - класс одного занятия (урока).
+
+Структура курса на уровне этих классов, приведена на рисунке ниже:
+
+
+
+Объекты класса LessonItem должны создаваться командой:
+
+lesson = LessonItem(название урока, число практических занятий, общая длительность урока)
+Соответственно, в каждом объекте класса LessonItem должны создаваться локальные атрибуты:
+
+title - название урока (строка);
+practices - число практических занятий (целое положительное число);
+duration - общая длительность урока (целое положительное число).
+
+Необходимо с помощью магических методов реализовать следующую логику взаимодействия с объектами класса LessonItem:
+
+1. Проверять тип присваиваемых данных локальным атрибутам. Если типы не соответствуют требованиям, то генерировать исключение командой:
+
+raise TypeError("Неверный тип присваиваемых данных.")
+2. При обращении к несуществующим атрибутам объектов класса LessonItem возвращать значение False.
+3. Запретить удаление атрибутов title, practices и duration в объектах класса LessonItem.
+
+Объекты класса Module должны создаваться командой:
+
+module = Module(название модуля)
+Каждый объект класса Module должен содержать локальные атрибуты:
+
+name - название модуля;
+lessons - список из уроков (объектов класса LessonItem), входящих в модуль (изначально список пуст).
+
+Также в классе Module должны быть реализованы методы:
+
+add_lesson(self, lesson) - добавление в модуль (в конец списка lessons) нового урока (объекта класса LessonItem);
+remove_lesson(self, indx) - удаление урока по индексу в списке lessons.
+
+Наконец, объекты класса Course создаются командой:
+
+course = Course(название курса)
+И содержат следующие локальные атрибуты:
+
+name - название курса (строка);
+modules - список модулей в курсе (изначально список пуст).
+
+Также в классе Course должны присутствовать следующие методы:
+
+add_module(self, module) - добавление нового модуля в конце списка modules;
+remove_module(self, indx) - удаление модуля из списка modules по индексу в этом списке.
+"""
+class LessonItem:
+    attrs = {"title":str, "practices":int, "duration":int}
+    
+    def __init__(self, title, practices, duration):
+        self.title = title
+        self.practices = practices
+        self.duration = duration
+        
+    def __setattr__(self, key, value):
+        if key in self.attrs:
+            if type(value) != self.attrs[key]:
+                raise TypeError("Неверный тип присваиваемых данных.")
+            if key == ("practices " or "duration ")  and value <= 0:
+                raise TypeError("Неверный тип присваиваемых данных.")
+                
+        object.__setattr__(self, key, value)
+            
+    def __getattr__(self, item):
+        return False
+        
+    def __delattr__(self, item):
+        if item == ("title" or "practices" or "duration"):
+            raise AttrbuteError()
+        
+        
+class Module:
+    def __init__(self, name):
+        self.name = name
+        self.lessons = []
+        
+    def add_lesson(self, lesson):
+        self.lessons.append(lesson)
+        
+    def remove_lesson(self, indx):
+        self.lessons.pop(indx)
+        
+        
+        
+class Course:
+    def __init__(self, name):
+        self.name = name
+        self.modules = []
+        
+    def add_module(self, module):
+        self.modules.append(module)
+        
+    def remove_module(self, indx):
+        self.modules.pop(indx)

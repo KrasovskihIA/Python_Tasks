872869fc540d07f37a77759f2b309c87d65fed96
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-02-09 21:32:55 +0400

    Update

--- PythonOOP_2.py
+++ PythonOOP_2.py
@@ -120,3 +120,40 @@ class MoneyD(Money):
 
 class MoneyE(Money):
     type_money = "euro"
+
+"""
+Необходимо объявить класс Body (тело), объекты которого создаются командой:
+
+body = Body(name, ro, volume)
+где name - название тела (строка); ro - плотность тела (число: вещественное или целочисленное); volume - объем тела  (число: вещественное или целочисленное).
+
+Для объектов класса Body должны быть реализованы операторы сравнения:
+
+body1 > body2  # True, если масса тела body1 больше массы тела body2
+body1 == body2 # True, если масса тела body1 равна массе тела body2
+body1 < 10     # True, если масса тела body1 меньше 10
+body2 == 5     # True, если масса тела body2 равна 5
+Масса тела вычисляется по формуле:
+
+m = ro * volume
+
+P.S. В программе только объявить класс, выводить на экран ничего не нужно.
+"""
+class Body:
+    def __init__(self, name, ro, volume):
+        self. name = name
+        self.ro = ro
+        self.volume = volume
+        self.massa = self.ro * self.volume
+
+    def __lt__(self, other):
+        if isinstance(other, Body):
+            return self.massa < other.massa
+        else:
+            return self.massa < other
+
+    def __eq__(self, other):
+        if isinstance(other, Body):
+            return self.massa == other.massa
+        else:
+            return self.massa == other

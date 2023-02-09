e87f9ee0f263371e6eef03a19b0647f4701ff9cf
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-02-09 21:05:09 +0400

    Update

--- PythonOOP.py
+++ PythonOOP.py
@@ -4789,32 +4789,27 @@ text = input()
 """
 class Morph:
     def __init__(self, *args):
-        self._words = list(map(lambda x: x.strip(" ?!:;,.").lower(), args))
+        self.words = args
 
     def add_word(self, word):
-        w = word.lower()
-        if w not in self._words:
-            self._words.append(w)
+        self.words += (word,)
 
     def get_words(self):
-        return tuple(self._words)
+        return self.words
 
-    def __eq__(self, value):
-        if type(value) != str:
-            raise ValueError("Error")
-        return value.lower() in self._words
+    def __eq__(self, other):
+        return other.strip('.,!-?').lower() in self.get_words()
 
+
+dict_words = [Morph('связь', 'связи', 'связью', 'связи', 'связей', 'связям', 'связями', 'связях'),
+              Morph('формула', 'формулы', 'формуле', 'формулу', 'формулой', 'формул', 'формулам', 'формулами',
+                    'формулах'),
+              Morph('вектор', 'вектора', 'вектору', 'вектором', 'векторе', 'векторы', 'векторов', 'векторам',
+                    'векторами', 'векторах'),
+              Morph('эффект', 'эффекта', 'эффекту', 'эффектом', 'эффекте', 'эффекты', 'эффектов', 'эффектам',
+                    'эффектами', 'эффектах'),
+              Morph('день', 'дня', 'дню', 'днем', 'дне', 'дни', 'дням', 'днями', 'днях')]
 
 text = input()
-
-dict_words = [Morph('связь', 'связи', 'связью', 'связей', 'связям', 'связями', 'связях'),
-              Morph('формула', 'формулы, формуле', 'формулу', 'формулой', 'формул', 'формулам', 'формулами', 'формулах'),
-              Morph('вектор', 'вектора', 'вектору', 'вектором', 'векторе', 'векторы', 'векторов', 'векторам', 'векторами', 'векторах'),
-              Morph('эффект', 'эффекта', 'эффекту', 'эффектом', 'эффекте', 'эффекты', 'эффектов', 'эффектам', 'эффектами', 'эффектах'),
-              Morph('день', 'дня, дню', 'днем', 'дне', 'дни', 'дням', 'днями', 'днях')
-] 
-
-
-words = map(lambda x: x.strip("?!:;,.").lower(), text.split())
-res = sum(word == morph for word in words for morph in dict_words)
-print (res)
+match = [word for word in text.split() for morph in dict_words if word == morph]
+print(len(match))
---
+++ PythonOOP_2.py
@@ -0,0 +1,122 @@
+"""
+Перед вами стоит задача выделения файлов с определенными расширениями из списка файлов, например:
+
+filenames = ["boat.jpg", "ans.web.png", "text.txt", "www.python.doc", "my.ava.jpg", "forest.jpeg", "eq_1.png", "eq_2.xls"]
+Для этого необходимо объявить класс FileAcceptor, объекты которого создаются командой:
+
+acceptor = FileAcceptor(ext1, ..., extN)
+где ext1, ..., extN - строки с допустимыми расширениями файлов, например: 'jpg', 'bmp', 'jpeg'.
+
+После этого предполагается использовать объект acceptor в стандартной функции filter языка Python следующим образом:
+
+filenames = list(filter(acceptor, filenames))
+То есть, объект acceptor должен вызываться как функция:
+
+acceptor(filename) 
+и возвращать True, если файл с именем filename содержит расширения, указанные при создании acceptor, и False - в противном случае. Кроме того, с объектами класса FileAcceptor должен выполняться оператор:
+
+acceptor12 = acceptor1 + acceptor2
+Здесь формируется новый объект acceptor12 с уникальными расширениями первого и второго объектов. Например:
+
+acceptor1 = FileAcceptor("jpg", "jpeg", "png")
+acceptor2 = FileAcceptor("png", "bmp")
+acceptor12 = acceptor1 + acceptor2    # ("jpg", "jpeg", "png", "bmp")
+Пример использования класса (эти строчки в программе писать не нужно):
+
+acceptor_images = FileAcceptor("jpg", "jpeg", "png")
+acceptor_docs = FileAcceptor("txt", "doc", "xls")
+filenames = list(filter(acceptor_images + acceptor_docs, filenames))
+P.S. На экран в программе ничего выводить не нужно.
+"""
+
+class FileAcceptor:
+    def __init__(self, *args):
+        self.file_names = list(args)
+
+    def __call__(self, filename):
+        return filename[filename.rfind('.')+1:] in self.file_names
+        
+    def __add__(self, other):
+        return FileAcceptor(*self.file_names + other.file_names)
+
+filenames = ["boat.jpg", "ans.web.png", "text.txt", "www.python.doc", "my.ava.jpg", "forest.jpeg", "eq_1.png", "eq_2.xls"]
+acceptor_images = FileAcceptor("jpg", "jpeg", "png")
+acceptor_docs = FileAcceptor("txt", "doc", "xls")
+filenames = list(filter(acceptor_images + acceptor_docs, filenames))
+
+
+"""
+ В программе необходимо объявить классы для работы с кошельками в разных валютах:
+
+MoneyR - для рублевых кошельков
+MoneyD - для долларовых кошельков
+MoneyE - для евро-кошельков
+"""
+class CentralBank:
+    rates = {'rub': 72.5, 'dollar': 1.0, 'euro': 1.15}
+
+    def __new__(cld, *args, **kwargs):
+        return
+    
+    @classmethod
+    def register(cls, money):
+        money.cb = cls
+
+
+class Money:
+    EPS = 0.1
+    type_money = None
+            
+    def __init__(self, volume=0, cb=None ):
+        self.__volume = volume
+        self.__cb = cb
+
+    @property
+    def volume(self):
+        return self.__volume
+
+    @volume.setter
+    def volume(self, other):
+        self.__volume = other
+
+    @property
+    def cb(self):
+        return self.__cb
+
+    @cb.setter
+    def cb(self, other):
+        self.__cb = other
+
+    def get_volumes(self, other):
+        if self.cb is None:
+            raise ValueError("Error")
+
+        if self.type_money is None:
+            raise ValueError("Error")
+
+        v1 = self.volume / self.cb.rates[self.type_money]
+        v2 = other.volume / self.cb.rates[other.type_money]
+        return v1, v2
+
+
+    def __eq__(self, other):
+        v1, v2 = self.get_volumes(other)
+        return abs(v1 - v2) < self.EPS
+
+    def __lt__(self, other):
+        v1, v2 = self.get_volumes(other)
+        return v1 < v2
+
+    def __le__(self, other):
+        v1, v2 = self.get_volumes(other)
+        return v1 <= v2
+
+
+class MoneyR(Money):
+    type_money = "rub"
+
+class MoneyD(Money):
+    type_money = "dollar"
+
+class MoneyE(Money):
+    type_money = "euro"

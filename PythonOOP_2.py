84cf3e951cdef633ca16a57381754ace09a65df7
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-02-17 22:19:04 +0400

    Update

--- PythonOOP_2.py
+++ PythonOOP_2.py
@@ -388,3 +388,54 @@ for i in lst_in:
     args = list(map(str.strip, i.split(';')))
     args[-1] = int(args[-1])
     db.write(Record(*args))
+
+
+
+"""
+Подвиг 8. Из входного потока необходимо прочитать список строк командой:
+
+lst_in = list(map(str.strip, sys.stdin.readlines()))
+Каждая строка содержит информацию об учебном пособии в формате:
+
+"Название; автор; год издания"
+
+Например:
+
+Python; Балакирев С.М.; 2020
+Python ООП; Балакирев С.М.; 2021
+Python ООП; Балакирев С.М.; 2022
+Python; Балакирев С.М.; 2021
+
+Необходимо каждую из этих строк представить объектом класса BookStudy, которые создаются командой:
+
+bs = BookStudy(name, author, year)
+где name - название пособия (строка); author - автор пособия (строка); year - год издания (целое число). Такие же публичные локальные атрибуты должны быть в объектах класса BookStudy.
+
+Для каждого объекта реализовать вычисление хэша по двум атрибутам: name и author (без учета регистра).
+
+Сформировать список lst_bs из объектов класса BookStudy на основе прочитанных строк (списка lst_in). После этого определить число книг с уникальными хэшами. Это число сохранить через переменную unique_books (целое число).
+
+P.S. На экран ничего выводить не нужно.
+"""
+import sys
+
+class BookStudy:
+    def __init__(self, name, author, year):
+        self.name = name
+        self.author = author
+        self.year = year
+
+    def __hash__(self):
+        return hash((self.name.lower(), self.author.lower()))
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
+lst_in = list(map(str.strip, sys.stdin.readlines()))
+lst_bs = list(map(lambda x: BookStudy(*x.split('; ')), lst_in))
+
+unique_books = len(set(lst_bs))
+
+
+
+

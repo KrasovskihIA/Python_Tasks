91445cd87cb61347f8d486a089a06f68a6502a36
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-02-19 17:22:47 +0400

    Update

--- PythonOOP_2.py
+++ PythonOOP_2.py
@@ -477,3 +477,76 @@ class Dimensions:
     
 s_inp = input()
 lst_dims = sorted([Dimensions(*map(float, elem.split())) for elem in s_inp.split('; ')], key=hash)
+
+"""
+Объявите класс с именем Triangle, объекты которого создаются командой:
+
+tr = Triangle(a, b, c)
+где a, b, c - длины сторон треугольника (числа: целые или вещественные). В классе Triangle объявите следующие дескрипторы данных:
+
+a, b, c - для записи и считывания длин сторон треугольника.
+
+При записи нового значения нужно проверять, что присваивается положительное число (целое или вещественное). Иначе, генерируется исключение командой:
+
+raise ValueError("длины сторон треугольника должны быть положительными числами")
+Также нужно проверять, что все три стороны a, b, c могут образовывать стороны треугольника. То есть, должны выполняться условия:
+
+a < b+c; b < a+c; c < a+b
+
+Иначе генерируется исключение командой:
+
+raise ValueError("с указанными длинами нельзя образовать треугольник")
+Наконец, с объектами класса Triangle должны выполняться функции:
+
+len(tr) - возвращает периметр треугольника, приведенный к целому значению с помощью функции int();
+tr() - возвращает площадь треугольника (можно вычислить по формуле Герона: s = sqrt(p * (p-a) * (p-b) * (p-c)), где p - полупериметр треугольника).
+
+P.S. На экран ничего выводить не нужно, только объявить класс Triangle.
+"""
+class PosotoveValue:           
+    def __set_name__(self, owner, name):
+        self.name = "_" + name
+ 
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name, None)
+ 
+    def __set__(self, instance, value):
+        if value <= 0 or type(value) not in (int, float):
+            raise ValueError("длины сторон треугольника должны быть положительными числами")
+        setattr(instance, self.name, value)
+
+
+class Triangle:
+    a = PosotoveValue()
+    b = PosotoveValue()
+    c = PosotoveValue()
+
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    @staticmethod
+    def __is_triangle(a, b, c):
+        if a and b and c:
+            return a < b+c and b < a+c and c < a+b
+        return True
+
+    def __setattr__(self, key, value):
+        if (key == 'a' and not self.__is_triangle(value, self.b, self.c)) or (key == 'b' and not self.__is_triangle(self.a, value, self.c)) or (key == 'c' and not self.__is_triangle(self.a, self.b, value)):
+            raise ValueError("с указанными длинами нельзя образовать треугольник")
+
+        object.__setattr__(self, key, value)
+
+
+    def __len__(self):
+        return int(self.a + self.b + self.c)
+
+
+    def __call__(self):
+        a, b, c =self.a, self.b, self.c
+        if not (a and b and c):
+            return
+
+        p = 0.5 * (a + b + c)
+        return (p * (p-a) * (p-b) * (p-c))**0.5

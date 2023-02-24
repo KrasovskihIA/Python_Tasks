eea1fa201da7af8a79f6f4dfaf777e14e6cd8b76
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-02-24 12:27:26 +0400

    Update

--- PythonOOP_2.py
+++ PythonOOP_2.py
@@ -831,3 +831,46 @@ class Vector:
     
     def __len__(self):
         return len(self.points)
+
+
+"""
+Подвиг 2. Объявите класс Record (запись), который описывает одну произвольную запись из БД. Объекты этого класса создаются командой:
+
+r = Record(field_name1=value1,... , field_nameN=valueN)
+где field_nameX - наименование поля БД; valueX - значение поля из БД.
+
+В каждом объекте класса Record должны автоматически создаваться локальные публичные атрибуты по именам полей (field_name1,... , field_nameN) с соответствующими значениями. Например:
+
+r = Record(pk=1, title='Python ООП', author='Балакирев')
+В объекте r появляются атрибуты:
+
+r.pk # 1
+r.title # Python ООП
+r.author # Балакирев
+Также необходимо обеспечить доступ к этим полям (чтение/запись) через индексы следующим образом:
+
+r[0] = 2 # доступ к полю pk
+r[1] = 'Супер курс по ООП' # доступ к полю title
+r[2] = 'Балакирев С.М.' # доступ к полю author
+print(r[1]) # Супер курс по ООП
+r[3] # генерируется исключение IndexError
+Если указывается неверный индекс (не целое число или некорректное целое число), то должно генерироваться исключение командой:
+
+raise IndexError('неверный индекс поля')
+P.S. В программе нужно объявить только класс. Выводить на экран ничего не нужно.
+
+P.P.S. Для создания локальных атрибутов используйте коллекцию __dict__ объекта класса Record.
+"""
+class Record:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def __getitem__(self, item):
+        if not isinstance(item, int) or item > len(list(self.__dict__.values())):
+            raise IndexError('неверный индекс поля')
+        return list(self.__dict__.values())[item]
+
+    def __setitem__(self, key, value):
+        if not isinstance(key, int) or key > len(list(self.__dict__.values())):
+            raise IndexError('неверный индекс поля')
+        self.__dict__[list(self.__dict__.keys())[key]] = value

c89af718da5d8ea8601ee6553027ce99d581a584
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-02-26 15:32:10 +0400

    Update

--- PythonOOP_2.py
+++ PythonOOP_2.py
@@ -1080,5 +1080,86 @@ class RadiusVector:
         self.coords[key] = value
 
 """
+одвиг 8. Вам нужно реализовать в программе игровое поле для игры "Крестики-нолики". Для этого требуется объявить класс TicTacToe (крестики-нолики), объекты которого создаются командой:
 
+game = TicTacToe()
+Каждый объект game должен иметь публичный атрибут:
+
+pole - игровое поле: кортеж размером 3х3 с объектами класса Cell.
+
+Каждая клетка игрового поля представляется объектом класса Cell и создается командой:
+
+cell = Cell()
+Объекты класса Cell должны иметь следующие публичные локальные атрибуты:
+
+is_free - True, если клетка свободна; False в противном случае;
+value - значение поля: 1 - крестик; 2 - нолик (по умолчанию 0).
+
+Также с каждым объектом класса Cell должна работать функция:
+
+bool(cell)
+которая возвращает True, если клетка свободна (cell.is_free=True) и False в противном случае.
+
+Класс TicTacToe должен иметь следующий метод:
+
+clear() - очистка игрового поля (все клетки заполняются нулями и переводятся в закрытое состояние);
+
+А объекты этого класса должны иметь следующую функциональность (обращение по индексам):
+
+game[0, 0] = 1 # установка нового значения, если поле закрыто
+res = game[1, 1] # получение значения центральной ячейки поля (возвращается число)
+Если указываются некорректные индексы, то должно генерироваться исключение командой:
+
+raise IndexError('неверный индекс клетки')
+Если идет попытка присвоить новое значение в открытую клетку поля, то генерировать исключение:
+
+raise ValueError('клетка уже занята')
+Также должны быть реализованы следующие полные срезы при обращении к клеткам игрового поля:
+
+slice_1 = game[:, indx] # выбираются все элементы (кортеж) столбца с индексом indx
+slice_2 = game[indx, :] # выбираются все элементы (кортеж) строки с индексом indx
 """
+#клетка игрового поля
+class Cell:
+    def __init__(self):
+        self.value = 0
+        self.is_free = True
+
+    def __bool__(self):
+        return self.is_free
+
+#игровое поле для игры "Крестики-нолики"
+class TicTacToe:
+    def __init__(self):
+        self.pole = tuple(tuple(Cell() for _ in range(3)) for _ in range(3))
+
+    def clear(self):
+        for row in self.pole:
+            for cell in row:
+                cell.is_free = True
+                cell.value = 0
+
+    def __check(self, item):
+        if type(item) != tuple or len(item) !=2:
+            raise IndexError('неверный индекс клетки')
+        if any(not (0 <= x < 3) for x in item if type(x) != slice):
+            raise IndexError('неверный индекс клетки')
+
+    def __setitem__(self, key, value):
+        self.__check(key)
+        r,c = key
+        if self.pole[r][c]:
+            self.pole[r][c].value = value
+            self.pole[r][c].is_free = False
+        else:
+           raise ValueError('клетка уже занята') 
+
+    def __getitem__(self, item):
+        self.__check(item)
+        r, c = item
+        if type(r) == slice:
+            return tuple(self.pole[x][c].value for x in range(3))
+        if type(c) == slice:
+            return tuple(self.pole[r][x].value for x in range(3))
+
+        return self.pole[r][c].value

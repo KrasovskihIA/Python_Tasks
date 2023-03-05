5b9701d302ae441b3646b7197432043bd1d9adda
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-03-05 18:29:22 +0400

    Update

--- PythonOOP_2.py
+++ PythonOOP_2.py
@@ -1393,5 +1393,32 @@ class TriangleListIterator:
 
 
 """
+Подвиг 7. Теперь, вам необходимо разработать итератор, который бы перебирал указанные столбцы двумерного списка. Список представляет собой двумерную таблицу из данных:
 
+lst = [[x11, x12, ..., x1N],
+       [x21, x22, ..., x2N],
+       ...
+       [xM1, xM2, ..., xMN]
+      ]
+Для этого в программе необходимо объявить класс с именем IterColumn, объекты которого создаются командой:
+
+it = IterColumn(lst, column)
+где lst - ссылка на двумерный список; column - индекс перебираемого столбца (отсчитывается от 0).
+
+Затем, с объектами класса IterColumn должны быть доступны следующие операции:
+
+it = IterColumn(lst, 1)
+for x in it:  # последовательный перебор всех элементов столбца списка: x12, x22, ..., xM2
+    print(x)
+
+it_iter = iter(it)
+x = next(it_iter)
 """
+class IterColumn:
+    def __init__(self, lst, column):
+        self._lst = lst
+        self._column = column
+
+    def __iter__(self):
+        for x in range(len(self._lst)):
+            yield self._lst[x][self._column]

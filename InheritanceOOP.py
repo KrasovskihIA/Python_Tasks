fd505608a4a4abb937b0177f1ff09518ceacfe22
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-03-14 17:59:06 +0400

    Update

--- InheritanceOOP.py
+++ InheritanceOOP.py
@@ -148,3 +148,25 @@ class DetailView(GenericView):
         f = getattr(self, method.lower(), False)
         if f :
             return f(request)
+
+"""
+
+"""
+class Singleton:
+    __instance = None
+    __instance_base = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls == Singleton:
+            if cls.__instance_base is None:
+                cls.__instance_base =  object().__new__(cls)
+            return cls.__instance_base
+            
+        if cls.__instance is None:
+            cls.__instance = object().__new__(cls)
+        return cls.__instance
+
+class Game(Singleton):
+    def __init__(self, name):
+        if 'name' not in self.__dict__:
+            self.name = name

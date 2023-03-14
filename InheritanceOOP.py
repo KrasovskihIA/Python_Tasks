1012707ae8f83218c4f8cf72f80940cb3d1da6c6
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-03-14 18:40:31 +0400

    Update

--- InheritanceOOP.py
+++ InheritanceOOP.py
@@ -150,7 +150,16 @@ class DetailView(GenericView):
             return f(request)
 
 """
-
+С помощью наследования можно как бы "наполнять" дочерние классы нужными качествами (свойствами). Как пример, объявите в программе класс с именем:
+Singleton
+который бы позволял создавать только один экземпляр (все последующие экземпляры должны ссылаться на первый). Как это делать, вы должны уже знать из этого курса.
+Затем, объявите еще один класс с именем:
+Game
+который бы наследовался от класса Singleton. Объекты класса Game должны создаваться командой:
+game = Game(name)
+где name - название игры (строка). В каждом объекте класса Game должен создаваться атрибут name с соответствующим содержимым.
+Убедитесь, что атрибут name принимает значение первого созданного объекта (если это не так, то поправьте инициализатор дочернего класса, чтобы это условие выполнялось).
+P.S. В программе нужно объявить только классы. Выводить на экран ничего не нужно.
 """
 class Singleton:
     __instance = None
@@ -161,7 +170,7 @@ class Singleton:
             if cls.__instance_base is None:
                 cls.__instance_base =  object().__new__(cls)
             return cls.__instance_base
-            
+
         if cls.__instance is None:
             cls.__instance = object().__new__(cls)
         return cls.__instance
@@ -170,3 +179,37 @@ class Game(Singleton):
     def __init__(self, name):
         if 'name' not in self.__dict__:
             self.name = name
+
+"""
+Вам необходимо создать множество классов для валидации (проверки) корректности данных. Для этого ваш непосредственный начальник (Senior) предлагает вам объявить в программе базовый класс с именем:
+Validator
+Проверка корректности выполняется с помощью метода _is_valid(). После этого, в программе нужно объявить два дочерних класса:
+
+IntegerValidator - для проверки, что data - целое число в заданном диапазоне;
+FloatValidator - для проверки, что data - вещественное число в заданном диапазоне.
+"""
+class Validator:
+    def __call__(self, data):
+        if not self._is_valid(data):
+            raise ValueError('данные не прошли валидацию')
+        return self._is_valid(data)
+      
+    def _is_valid(self, data):
+        pass
+
+class IntegerValidator(Validator):  # для проверки, что data - целое число в заданном диапазоне
+    def __init__(self, min_value, max_value):
+        self.min_value = min_value
+        self.max_value = max_value
+            
+    def _is_valid(self, data):        
+        return isinstance(data, int) and self.min_value <= data <= self.max_value
+    
+class FloatValidator(Validator):  # для проверки, что data - вещественное число в заданном диапазоне
+    def __init__(self, min_value, max_value):
+        self.min_value = min_value
+        self.max_value = max_value
+            
+    def _is_valid(self, data):    
+        return isinstance(data, float) and self.min_value <= data <= self.max_value
+    

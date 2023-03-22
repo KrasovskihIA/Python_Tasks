3092fe6cef02b95aeb1a6c3f4ed2c933b08e66d1
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-03-22 19:41:45 +0400

    Update

--- InheritanceOOP.py
+++ InheritanceOOP.py
@@ -264,3 +264,64 @@ class NetworkIterator:
         while layer:
             yield layer
             layer = layer.next_layer
+
+"""
+Объявите в программе класс Vector, объекты которого создаются командой:
+
+v = Vector(x1, x2, ..., xN)
+где x1, x2, ..., xN - координаты радиус-вектора (числа: целые или вещественные).
+
+С объектами этого класса должны выполняться команды:
+
+v1 = Vector(1, 2, 3)
+v2 = Vector(3, 4, 5)
+v = v1 + v2 # формируется новый вектор (объект класса Vector) с соответствующими координатами
+v = v1 - v2 # формируется новый вектор (объект класса Vector) с соответствующими координатами
+Если размерности векторов v1 и v2 не совпадают, то генерировать исключение:
+
+raise TypeError('размерности векторов не совпадают')
+В самом классе Vector объявите метод с именем get_coords, который возвращает кортеж из текущих координат вектора.
+
+На основе класса Vector объявите дочерний класс VectorInt для работы с целочисленными координатами:
+
+v = VectorInt(1, 2, 3, 4)
+v = VectorInt(1, 0.2, 3, 4) # ошибка: генерируется исключение raise ValueError('координаты должны быть целыми числами')
+При операциях сложения и вычитания с объектом класса VectorInt:
+
+v = v1 + v2  # v1 - объект класса VectorInt
+v = v1 - v2  # v1 - объект класса VectorInt
+должен формироваться объект v как объект класса Vector, если хотя бы одна координата является вещественной. Иначе, v должен быть объектом класса VectorInt.
+
+P.S. В программе нужно объявить только классы. Выводить на экран ничего не нужно.
+"""
+class Vector:
+    def __init__(self, *args):
+        self.coords = args
+
+    def __make_vector(self, coords):
+        try:
+            return self.__class__(*coords)
+        except ValueError:
+            return Vector(*coords)
+
+    def __add__(self, other):
+        if len(self.coords) != len(other.coords):
+            raise TypeError('размерности векторов не совпадают')
+        coords = tuple(map(sum, zip(self.coords, other.coords)))
+        return self.__make_vector(coords)
+
+    def __sub__(self, other):
+        if len(self.coords) != len(other.coords):
+            raise TypeError('размерности векторов не совпадают')
+        coords = tuple(x - y for (x, y) in zip(self.coords, other.coords))
+        return self.__make_vector(coords)
+
+    def get_coords(self):
+        return self.coords
+
+
+class VectorInt(Vector):
+    def __init__(self, *args):
+        super().__init__(*args)
+        if not all(type(i) is int for i in args):
+            raise ValueError('координаты должны быть целыми числами')

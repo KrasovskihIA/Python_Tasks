21667de9d0f32cb4b6ddcf13c3f18647b8888752
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-04-02 20:21:34 +0400

    Update

--- InheritanceOOP.py
+++ InheritanceOOP.py
@@ -817,3 +817,42 @@ class Callback:
 
     def __call__(self, func):
         self.__router_cls.add_callback(self.__path, func)
+
+"""
+Подвиг 7. В программе объявлена функция integer_params для класса Vector, которая применяет к каждому методу класса декоратор integer_params_decorated:
+Декоратор integer_params_decorated должен проверять, чтобы все передаваемые аргументы в методы класса (кроме первого self) были целыми числами (имели тип int). Если это не так, то должно генерироваться исключение командой:
+
+raise TypeError("аргументы должны быть целыми числами")
+Ваша задача объявить эту функцию-декоратор.
+"""
+def integer_params_decorated(func):
+    def wrapper(self, *args, **kwargs):
+        if not all(type(x) == int for x in args):
+            raise TypeError("аргументы должны быть целыми числами")
+        if not all(type(x) == int for x in kwargs.values()):
+            raise TypeError("аргументы должны быть целыми числами")
+        return func(self, *args, **kwargs)
+    return wrapper
+
+def integer_params(cls):
+    methods = {k: v for k, v in cls.__dict__.items() if callable(v)}
+    for k, v in methods.items():
+        setattr(cls, k, integer_params_decorated(v))
+
+    return cls
+
+
+@integer_params
+class Vector:
+    def __init__(self, *args):
+        self.__coords = list(args)
+
+    def __getitem__(self, item):
+        return self.__coords[item]
+
+    def __setitem__(self, key, value):
+        self.__coords[key] = value
+
+    def set_coords(self, *coords, reverse=False):
+        c = list(coords)
+        self.__coords = c if not reverse else c[::-1]

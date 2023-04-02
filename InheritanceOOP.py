ebda2135507722188c7438a8cfd41fb55ab640e4
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-04-02 20:44:20 +0400

    Update

--- InheritanceOOP.py
+++ InheritanceOOP.py
@@ -856,3 +856,13 @@ class Vector:
     def set_coords(self, *coords, reverse=False):
         c = list(coords)
         self.__coords = c if not reverse else c[::-1]
+
+"""
+Подвиг 8 (на повторение). Объявите класс SoftList, который наследуется от стандартного класса list. В классе SoftList следует объявить необходимые магические методы так, чтобы при обращении к несуществующему элементу (по индексу) возвращалось значение False (а не исключение Out of Range). Например:
+"""
+class SoftList(list):
+    def __getitem__(self, item):
+        try:
+            return super().__getitem__(item)
+        except IndexError:
+            return False

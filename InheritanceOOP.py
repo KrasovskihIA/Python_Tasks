9dbf3f037db211756b00d9e18376140c7a8f548d
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-04-04 18:09:26 +0400

    Update

--- InheritanceOOP.py
+++ InheritanceOOP.py
@@ -900,3 +900,33 @@ class StringDigit(str):
         if not value.isdigit():
             raise ValueError("в строке должны быть только цифры")
         return StringDigit(value + self.string )
+
+
+"""
+Объявите базовый класс с именем ItemAttrs, который бы позволял обращаться к локальным атрибутам объектов дочерних классов по индексу. Для этого в классе ItemAttrs нужно переопределить следующие методы:
+
+__getitem__() - для получения значения атрибута по индексу;
+__setitem__() - для изменения значения атрибута по индексу.
+
+Объявите дочерний класс Point для представления координаты точки на плоскости. Объекты этого класса должны создаваться командой:
+
+pt = Point(x, y)
+где x, y - целые или вещественные числа.
+"""
+class ItemAttrs:
+    def __init__(self, *args):
+        self.coords = list(args)
+
+    def __getitem__(self, item):
+        return self.coords[item]
+
+    def __setitem__(self, key, value):
+        self.coords[key] = value
+
+class Point(ItemAttrs):
+    pass
+
+pt = Point(1, 2.5)
+x = pt[0]   # 1
+y = pt[1]   # 2.5
+pt[0] = 10

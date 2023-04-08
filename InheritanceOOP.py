160e68bc0d8994fe5cb3466150fbcfa962ed3060
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-04-08 18:23:25 +0400

    Update

--- InheritanceOOP.py
+++ InheritanceOOP.py
@@ -1450,3 +1450,82 @@ class ModelForm(Model):
 
     def get_pk(self):
         return self._id
+
+"""
+Используя информацию о модуле abc из предыдущего подвига 6, объявите базовый класс с именем StackInterface со следующими абстрактными методами:
+
+def push_back(self, obj) - добавление объекта в конец стека;
+def pop_back(self) - удаление последнего объекта из стека.
+На основе этого класса объявите дочерний класс с именем Stack. Объекты этого класса должны создаваться командой:
+
+st = Stack()
+и в каждом объекте этого класса должен формироваться локальный атрибут:
+
+_top - ссылка на первый объект стека (для пустого стека _top = None).
+
+В самом классе Stack переопределить абстрактные методы базового класса:
+
+def push_back(self, obj) - добавление объекта в конец стека;
+def pop_back(self) - удаление последнего объекта из стека.
+
+Сами объекты стека должны определяться классом StackObj и создаваться командой:
+
+obj = StackObj(data)
+где data - информация, хранящаяся в объекте (строка). В каждом объекте класса StackObj должны автоматически формироваться атрибуты:
+
+_data - информация, хранящаяся в объекте (строка);
+_next - ссылка на следующий объект стека (если следующий отсутствует, то _next = None).
+"""
+from abc import ABC, abstractmethod
+
+
+class StackInterface(ABC):
+    @abstractmethod
+    def push_back(self, obj):
+        pass
+
+    @abstractmethod
+    def pop_back(self):
+        pass
+
+class StackObj:
+    def __init__(self, data):
+        self._data = data
+        self._next = None
+
+    @property
+    def next(self):
+        return self._next
+
+    @next.setter
+    def next(self, value):
+        self._next = value
+
+class Stack(StackInterface):
+    def __init__(self):
+        self._top = self._tail = None
+
+    def __get_pre(self):
+        pre = self._top
+        while pre.next.next:
+            pre = pre.next
+        return pre
+
+    def push_back(self, obj):
+        if self._tail:
+            self._tail.next = obj
+            self._tail = obj
+        else:
+            self._top = self._tail = obj
+
+    def pop_back(self):
+        if self._top == self._tail:
+            res = self._tail
+            self._top = self._tail = None
+            return res
+
+        pre = self.__get_pre()
+        last = self._tail
+        self._tail = pre
+        pre.next = None
+        return last

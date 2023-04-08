b62a13889445172e70b5f2b7beff8c639be2aaa5
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-04-08 19:00:57 +0400

    Update

--- InheritanceOOP.py
+++ InheritanceOOP.py
@@ -1599,3 +1599,76 @@ class Country(CountryInterface):
 
     def get_info(self):
         return f"{self._name}: {self._square}, {self._population}"
+
+"""
+Вам поручают разработать класс для представления маршрутов в навигаторе. Для этого требуется объявить класс с именем Track, объекты которого могут создаваться командами:
+
+tr = Track(start_x, start_y)
+tr = Track(pt1, pt2, ..., ptN)
+где start_x, start_y - начальная координата маршрута (произвольные числа); pt1, pt2, ..., ptN - набор из произвольного числа точек (координат) маршрута (объекты класса PointTrack).
+
+При передаче аргументов (start_x, start_y) координата должна представляться первым объектом класса PointTrack. Наборы всех точек (объектов PointTrack) должны сохраняться в локальном приватном атрибуте объекта класса Track:
+
+__points - список из точек (координат) маршрута.
+
+Далее, каждая точка (координата) должна определяться классом PointTrack, объекты которого создаются командой:
+
+pt = PointTrack(x, y)
+где x, y - числа (целые или вещественные). Если передается другой тип данных, то должно генерироваться исключение командой:
+
+raise TypeError('координаты должны быть числами')
+В классе PointTrack переопределите магический метод __str__, чтобы информация об объекте класса возвращалась в виде строки:
+
+"PointTrack: <x>, <y>"
+В самом классе Track должно быть свойство (property) с именем:
+
+points - для получения кортежа из точек маршрута.
+
+Также в классе Track должны быть методы:
+
+def add_back(self, pt) - добавление новой точки в конец маршрута (pt - объект класса PointTrack);
+def add_front(self, pt) - добавление новой точки в начало маршрута (pt - объект класса PointTrack);
+def pop_back(self) - удаление последней точки из маршрута;
+def pop_front(self) - удаление первой точки из маршрута.
+"""
+class Track:
+    def __init__(self, *args):
+        self.__points = list(args)
+    
+    def __setattr__(self, name, val):
+        if isinstance(val[0], PointTrack):            
+            super().__setattr__(name, val)
+        else:            
+            super().__setattr__(name, [PointTrack(val[0], val[1])])
+            
+    @property
+    def points(self):
+        return tuple(self.__points)
+    
+    def add_back(self, pt):
+        self.__points.append(pt)
+
+    def add_front(self, pt):
+        self.__points.insert(0, pt)
+
+    def pop_back(self):
+        self.__points.pop()
+
+    def pop_front(self):
+        self.__points.pop(0)
+        
+    
+    
+class PointTrack:
+    def __init__(self, x, y):
+        self.valid(x, y)
+        self.x = x
+        self.y = y
+    
+    @staticmethod
+    def valid(x, y):
+        if type(x) not in (int, float) or type(y) not in (int, float):
+            raise TypeError('координаты должны быть числами')
+        
+    def __str__(self):
+        return f'PointTrack: {self.x}, {self.y}'

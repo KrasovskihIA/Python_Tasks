a852007206801f128a42d25b71ee61c8eb9976d6
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-04-09 19:35:55 +0400

    Update

--- InheritanceOOP.py
+++ InheritanceOOP.py
@@ -1885,3 +1885,51 @@ class GeneralView:
 
 class DetailView(RetriveMixin, UpdateMixin, GeneralView):
     allowed_methods = ('GET', 'POST', )
+
+"""
+Вам необходимо добавить в класс MoneyOperators аналогичную реализацию оператора вычитания.
+"""
+class Money:
+    def __init__(self, value):
+        self._money = value
+
+    def __setattr__(self, key, value):
+        if type(value) != (int or float):
+            raise TypeError('сумма должна быть числом')
+        object.__setattr__(self, key, value)
+
+    @property
+    def money(self):
+        return self._money
+
+    @money.setter
+    def money(self, value):
+        self._money = value
+
+class MoneyOperators:
+    def __add__(self, other):
+        if type(other) in (int, float):
+            return self.__class__(self.money + other)
+
+        if type(self) != type(other):
+            raise TypeError('Разные типы объектов')
+
+        return self.__class__(self.money + other.money)
+
+    def __sub__(self, other):
+        if type(other) in (int, float):
+            return self.__class__(self.money - other)
+
+        if type(self) != type(other):
+            raise TypeError('Разные типы объектов')
+
+        return self.__class__(self.money - other.money)
+
+class MoneyR(Money, MoneyOperators):
+    def __str__(self):
+        return f"MoneyR: {self.money}"
+
+
+class MoneyD(Money, MoneyOperators):
+    def __str__(self):
+        return f"MoneyD: {self.money}"

069d524f3e3da99d17f3b827af07620fed6d6e69
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-04-12 20:33:21 +0400

    Update

--- InheritanceOOP.py
+++ InheritanceOOP.py
@@ -2125,3 +2125,37 @@ class Notes:
         
 
 notes = Notes()
+
+
+"""
+
+"""
+class Function:
+    def __init__(self):
+        self._amplitude = 1.0     # амплитуда функции
+        self._bias = 0.0          # смещение функции по оси Oy
+
+    def __call__(self, x, *args, **kwargs):
+        return self._amplitude * self._get_function(x) + self._bias
+
+    def _get_function(self, x):
+        raise NotImplementedError('метод _get_function должен быть переопределен в дочернем классе')
+
+    def __add__(self, other):
+        if type(other) not in (int, float):
+            raise TypeError('смещение должно быть числом')
+
+        obj = self.__class__(self)
+        obj._bias = self._bias + other
+        return obj
+
+    def __mul__(self, other):
+        if type(other) not in (int, float):
+            raise TypeError('смещение должно быть числом')
+
+        obj = self.__class__(self)
+        obj._bias = self._bias * other
+        return obj
+
+
+# здесь объявляйте класс Linear

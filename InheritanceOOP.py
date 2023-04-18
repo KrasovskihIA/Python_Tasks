77de9e697ea688f27c2cc48642c1099667e2ec25
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-04-18 20:51:35 +0400

    Update

--- InheritanceOOP.py
+++ InheritanceOOP.py
@@ -2651,3 +2651,66 @@ try:
 except ValueError as e:
     print(e)
 
+"""
+Подвиг 4. Объявите класс-исключение с именем StringException, унаследованным от базового класса Exception. После этого объявите еще два класса-исключения:
+
+NegativeLengthString - ошибка, если длина отрицательная;
+ExceedLengthString - ошибка, если длина превышает заданное значение;
+
+унаследованные от базового класса StringException.
+
+Затем, в блоке try (см. программу) пропишите команду генерации исключения для перехода в блок обработки исключения ExceedLengthString.
+"""
+class StringException(Exception):
+    pass
+
+class NegativeLengthString(StringException):
+    pass
+
+class ExceedLengthString(StringException):
+    pass
+
+try:
+    raise ExceedLengthString()
+except NegativeLengthString:
+    print("NegativeLengthString")
+except ExceedLengthString:
+    print("ExceedLengthString")
+except StringException:
+    print("StringException")
+
+
+"""
+Подвиг 5. Объявите в программе класс-исключение с именем PrimaryKeyError, унаследованным от базового класса Exception. Объекты класса PrimaryKeyError должны создаваться командами:
+
+e1 = PrimaryKeyError()          # Первичный ключ должен быть целым неотрицательным числом
+e2 = PrimaryKeyError(id='abc')  # Значение первичного ключа id = abc недопустимо
+e3 = PrimaryKeyError(pk='123')  # Значение первичного ключа pk = 123 недопустимо
+В первом варианте команды должно формироваться сообщение об ошибке "Первичный ключ должен быть целым неотрицательным числом". При втором варианте:
+
+"Значение первичного ключа id = <id> недопустимо"
+
+И при третьем:
+
+"Значение первичного ключа pk = <pk> недопустимо"
+
+Эти сообщения должны формироваться при отображении объектов класса PrimaryKeyError, например:
+
+print(e2) # Значение первичного ключа id = abc недопустимо
+Затем, сгенерируйте это исключение с аргументом id = -10.5, обработайте его и отобразите на экране объект исключения.
+"""
+class PrimaryKeyError(Exception):
+    def __init__(self, **kwargs):
+        if "id" not in kwargs and "pk" not in kwargs:
+            self.message = "Первичный ключ должен быть целым неотрицательным числом"
+        else:
+            key, value = tuple(kwargs.items())[0]
+            self.message = f"Значение первичного ключа {key} = {value} недопустимо"
+
+    def __str__(self):
+        return self.message
+
+try:
+    raise PrimaryKeyError(id = -10.5)
+except PrimaryKeyError as e:
+    print (e)
